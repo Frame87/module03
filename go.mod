1f6e5476ca933e1f6a681d38f667654f356d9d23
Mikhail Pirozhnikov <mikhail.pirozhnikov@tele2.kz>
2025-09-24 17:45:15 +0500

    Add v2 support with InSliceInt

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module github.com/Frame87/module03/v2
+module github.com/Frame87/module03
 
 go 1.14
+
+require (
+    github.com/Frame87/module03/utils v1.0.0
+    github.com/Frame87/module03/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"github.com/Frame87/module03/utils"
+	utilsV2 "github.com/Frame87/module03/v2"
 )
 
 func main() {
+	// Использование оригинальной версии
+	fmt.Println("Original utils version:")
 	stringSlice := []string{"Hello", "World", "Go"}
 	fmt.Println("Contains 'World':", utils.Contains(stringSlice, "World"))
 
 	intSlice := []int{1, 2, 3, 4, 5}
 	fmt.Println("ContainsInt 3:", utils.ContainsInt(intSlice, 3))
-	fmt.Println("InSliceInt 3:", utils.InSliceInt(intSlice, 3))
+
+	// Использование второй версии
+	fmt.Println("\nUtils v2 version:")
+	fmt.Println("InSliceInt 3:", utilsV2.InSliceInt(intSlice, 3))
+	fmt.Println("InSliceInt 6:", utilsV2.InSliceInt(intSlice, 6))
 }
